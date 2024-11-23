ba5f8a6c9b9a4303b97eb827e6b538fe1545660f
Irsyad Al Ghifary <100660393+irsyaddd@users.noreply.github.com>
2024-11-23 18:38:21 +0700

    Update fancy-button.tsx

--- src/components/fancy-button.tsx
+++ src/components/fancy-button.tsx
@@ -6,12 +6,29 @@ export default function FancyButton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  const downloadLink = 'https://drive.google.com/uc?export=download&id=1xybAOngpDJdOAtGeSbBZNDR--ZjCXlzh'
-  const viewLink = 'https://drive.google.com/file/d/1xybAOngpDJdOAtGeSbBZNDR--ZjCXlzh/view?usp=sharing'
+  const downloadLink = "https://drive.google.com/uc?export=download&id=1xybAOngpDJdOAtGeSbBZNDR--ZjCXlzh";
+  const viewLink = "https://drive.google.com/file/d/1xybAOngpDJdOAtGeSbBZNDR--ZjCXlzh/view?usp=sharing";
   return (
-    <div className={cn(className,"dark:bg-black/20 bg-black p-[.1rem] rounded-md")}>
-      <Button  className="bg-[#1d1f23] relative overflow-hidden drop-shadow-lg shadow-black/20 dark:text-white border-t-[1.5px] border-t-white/30 active:border-t-white/0 tracking-tighter">
-        <div className="absolute inset-0 bg-gradient-to-b from-white/10 w-full h-full active:hidden transition duration-100 ease-in-out" />
+    <div
+      className={cn(
+        className,
+        "dark:bg-black/20 bg-black p-[.1rem] rounded-md"
+      )}
+    >
+      <Button
+        onClick={() => window.open(viewLink, "_blank")}
+        className={cn(
+          "bg-[#1d1f23] relative overflow-hidden drop-shadow-lg",
+          "shadow-black/20 dark:text-white border-t-[1.5px]",
+          "border-t-white/30 active:border-t-white/0 tracking-tighter"
+        )}
+      >
+        <div
+          className={cn(
+            "absolute inset-0 bg-gradient-to-b from-white/10",
+            "w-full h-full active:hidden transition duration-100 ease-in-out",
+          )}
+        />
         View Resume
       </Button>
     </div>
